fe78081f508b8fdffc6031425dfb5b62ad23437d
oli <oli.loades@gmail.com>
2018-09-03 14:20:44 +0100

    red

---
+++ tasks/tasks/src/test/java/FanilyTree/Tests/FamilyTreeTest.java
@@ -0,0 +1,72 @@
+package FanilyTree.Tests;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FamilyTreeTest {
+    private Family fam;
+
+    @Before
+    public void setup(){
+        fam = new Family();
+    }
+
+    @Test
+    public void addTest(){//genderless
+        assertEquals(true,fam.add("bob"));
+        assertEquals(false,fam.add("bob"));
+    }
+
+    @Test
+    public void maleTest(){//sets male
+        assertEquals(true,fam.male("bob"));
+        assertEquals(false,fam.male("bob"));
+    }
+
+    @Test
+    public void femaleTest(){//sets female
+        assertEquals(true,fam.female("sue"));
+        assertEquals(false,fam.female("sue"));
+    }
+
+    @Test
+    public void isMaleTest(){//checks if male
+        fam.male("bob");
+        fam.female("sue");
+        fam.add("rob");
+        assertEquals(true,fam.isMale("bob"));
+        assertEquals(false,fam.isMale("sue"));
+        assertEquals(false,fam.isMale("rob"));
+    }
+
+    @Test
+    public void isFemaleTest(){//checks if female
+        fam.male("bob");
+        fam.female("sue");
+        fam.add("rob");
+        assertEquals(false,fam.isMale("bob"));
+        assertEquals(true,fam.isMale("sue"));
+        assertEquals(false,fam.isMale("rob"));
+    }
+
+    @Ignore
+    @Test
+    public void setParentTest(){
+
+    }
+
+    @Ignore
+    @Test
+    public void getParentTest(){
+
+    }
+
+    @Ignore
+    @Test
+    public void getChildrenTest(){
+
+    }
+}
