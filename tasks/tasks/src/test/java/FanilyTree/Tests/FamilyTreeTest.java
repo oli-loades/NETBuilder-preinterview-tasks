0046ebd35a4d94f8eb560d9f9687f32ef9f9d426
oli <oli.loades@gmail.com>
2018-09-04 20:34:48 +0100

    removed ignore from unit tests

--- tasks/tasks/src/test/java/FanilyTree/Tests/FamilyTreeTest.java
+++ tasks/tasks/src/test/java/FanilyTree/Tests/FamilyTreeTest.java
@@ -18,43 +18,43 @@ public class FamilyTreeTest {
         fam = new Family();
     }
 
-//    @Test
-//    public void addTest(){//genderless
-//        assertEquals(true,fam.add("bob"));
-//        assertEquals(false,fam.add("bob"));
-//    }
-//
-//    @Test
-//    public void maleTest(){//sets male
-//        assertEquals(true,fam.male("bob"));
-//        assertEquals(false,fam.male("bob"));
-//    }
-//
-//    @Test
-//    public void femaleTest(){//sets female
-//        assertEquals(true,fam.female("sue"));
-//        assertEquals(false,fam.female("sue"));
-//    }
-//
-//    @Test
-//    public void isMaleTest(){//checks if male
-//        fam.male("bob");
-//        fam.female("sue");
-//        fam.add("rob");
-//        assertEquals(true,fam.isMale("bob"));
-//        assertEquals(false,fam.isMale("sue"));
-//        assertEquals(false,fam.isMale("rob"));
-//    }
-//
-//    @Test
-//    public void isFemaleTest(){//checks if female
-//        fam.male("bob");
-//        fam.female("sue");
-//        fam.add("rob");
-//        assertEquals(false,fam.isFemale("bob"));
-//        assertEquals(true,fam.isFemale("sue"));
-//        assertEquals(false,fam.isFemale("rob"));
-//    }
+    @Test
+    public void addTest(){//genderless
+        assertEquals(true,fam.add("bob"));
+        assertEquals(false,fam.add("bob"));
+    }
+
+    @Test
+    public void maleTest(){//sets male
+        assertEquals(true,fam.male("bob"));
+        assertEquals(false,fam.male("bob"));
+    }
+
+    @Test
+    public void femaleTest(){//sets female
+        assertEquals(true,fam.female("sue"));
+        assertEquals(false,fam.female("sue"));
+    }
+
+    @Test
+    public void isMaleTest(){//checks if male
+        fam.male("bob");
+        fam.female("sue");
+        fam.add("rob");
+        assertEquals(true,fam.isMale("bob"));
+        assertEquals(false,fam.isMale("sue"));
+        assertEquals(false,fam.isMale("rob"));
+    }
+
+    @Test
+    public void isFemaleTest(){//checks if female
+        fam.male("bob");
+        fam.female("sue");
+        fam.add("rob");
+        assertEquals(false,fam.isFemale("bob"));
+        assertEquals(true,fam.isFemale("sue"));
+        assertEquals(false,fam.isFemale("rob"));
+    }
 
     @Test
     public void setParentTest(){
